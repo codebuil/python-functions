c5e52ddf7a4b95501c7722b2a5791f175385df9e
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-11-14 17:29:56 +0000

    Add files via upload

--- ram.py
+++ ram.py
@@ -2,9 +2,11 @@
 def setram(ramr):
 	n=0;
 	for n in range(0,12):
-		ramr=ramr+[n];
+		ramr=ramr+[0];
 	return ramr;
-def ram(indexs):
+def wram(ramr,indexs,value):
+	ramr[indexs]=value;
+def ram(ramr,indexs):
 	if indexs==0:
 		return len(ramr);
 	return ramr[indexs-1];
@@ -13,6 +15,8 @@ n=0;
 number=0;
 ramr=setram(ramr);
 print("\033c\033[42;30m\n");
-number=ram(0);
+number=ram(ramr,0);
 for n in range(0,number):
-	print ram(n+1);
+	wram(ramr,n,n);
+for n in range(0,number):
+	print ram(ramr,n+1);
