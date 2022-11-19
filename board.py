053cd87d9d10f1aaff86b7e74223ef9b3310c653
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-11-19 12:28:09 +0000

    Add files via upload

--- board.py
+++ board.py
@@ -13,19 +13,33 @@ root=Tk()
 ss=StringVar()
 ss.set(" ")
 sbitmaps=Canvas(root,bg="green",height=300,width=500)
-for y in range(0,8):
-	for x in range(0,8):
-		rets=rets+[sbitmaps.create_rectangle(x*20,y*20,x*20+18,y*20+18,fill="white")]
-
-
+def setram():
+	y=0
+	x=0
+	global rets
+	for y in range(0,8):
+		for x in range(0,8):
+			rets=rets+[sbitmaps.create_rectangle(x*20,y*20,x*20+18,y*20+18,fill="white")]
+def wram(indexs,value):
+	global rets	
+	if value==0:
+		sbitmaps.itemconfig(rets[indexs],fill="black")
+	if value!=0:
+		sbitmaps.itemconfig(rets[indexs],fill="white")	
 def checkers():
     global t
+    global rets
+    setram()
     counter=0;
+    counter2=0;
     while t > 0:
-        time.sleep(2)
+        time.sleep(0.25)
 	if counter>len(rets)-2:
-		t=0;
-	sbitmaps.itemconfig(rets[counter],fill="black")
+		counter=0;
+		counter2+=1;
+		if counter2>1:
+			counter2=0;
+	wram(counter,counter2);
 	counter+=1
     return 
 
