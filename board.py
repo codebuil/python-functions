ed78e2de5ed0d65e8624bc0fb535a8aaa04e5fdd
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-11-19 13:09:31 +0000

    Add files via upload

--- board.py
+++ board.py
@@ -21,29 +21,33 @@ def setram():
 	for y in range(0,8):
 		for x in range(0,8):
 			rets=rets+[sbitmaps.create_rectangle(x*20,y*20,x*20+18,y*20+18,fill="white")]
-def wram(indexs,value):
+def wram(indexs,x,y,value):
 	global rets	
-	if value==0:
-		sbitmaps.itemconfig(rets[indexs],fill="black")
-	if value!=0:
-		sbitmaps.itemconfig(rets[indexs],fill="white")	
+	if x<8 and y <8 and value==0:
+		sbitmaps.itemconfig(rets[y*8+x],fill="black")
+	if x<8 and y <8 and value!=0:
+		sbitmaps.itemconfig(rets[y*8+x],fill="white")	
 def checkers():
     global t
     global tt
     global rets
     setram()
-    counter=0;
-    counter2=0;
+    xx=0;
+    yy=0;
+    colors=0
     while t > 0:
         time.sleep(0.25)
-        if counter>len(rets)-1:
-            counter=0;
-            counter2+=1;
-            if counter2>1:
-                counter2=0;
+        if xx>8:
+            xx=0;
+            yy+=1;
+            if yy>8:
+                yy=0;
+                colors+=1;
+            if colors>1:
+                colors=0
         if t==1:
-            wram(counter,counter2);
-        counter+=1
+            wram(counter,xx,yy,colors);
+        xx+=1
     tt=0
     print(tt)
     return
