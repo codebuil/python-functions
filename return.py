4a1413264bad4b9b877c88998a92ebe0c3f8f7cb
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-11-20 16:50:11 +0000

    Add files via upload

---
+++ return.py
@@ -0,0 +1,45 @@
+import random;
+import time;
+import thread;
+from Tkinter import *
+import tkMessageBox;
+rets=[];
+tt=1
+#change bord w
+maxx=120
+winx=500
+#change bord h
+maxy=120
+winy=300
+t=1
+root=Tk()
+sbitmaps=Canvas(root,bg="green",height=winy,width=winx)
+def getn(maxs):
+	return random.randrange(0,maxs,1)
+def centers(ww,b):
+	return (ww/2)-(b/2)
+def sets(winx,winy):
+	global rets
+	n=0;
+	nx=winx
+	ny=winy
+	while nx >0:
+		rets=rets+[sbitmaps.create_rectangle(centers(winx,nx),centers(winy,ny),centers(winx,nx)+nx,centers(winy,ny)+ny,fill="green")]
+		print (nx)
+		print (ny)
+		ny=int(ny/2);
+		nx=int(nx/2);
+		if ny<16 or nx<16:
+			ny=0;
+			nx=0;
+def checkers():
+    global t
+    tt=0;
+    sets(winx,winy)
+    while t >0:
+        tt=0;
+sbitmaps.pack() 
+thread.start_new_thread(checkers,())
+root.mainloop()            
+t=0
+time.sleep(1)
